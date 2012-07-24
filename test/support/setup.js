531aafcbf05ca98e5fa1415de455b06dfa2aa80f
Domenic Denicola <domenic@domenicdenicola.com>
2012-07-24 09:48:01 -0400

    Use Mocha as Promised in tests.

--- test/support/setup.js
+++ test/support/setup.js
@@ -1,6 +1,9 @@
+var mochaAsPromised = require("mocha-as-promised");
 var chai = require("chai");
 var sinonChai = require("sinon-chai");
 var chaiAsPromised = require("chai-as-promised");
+
+mochaAsPromised();
 
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
